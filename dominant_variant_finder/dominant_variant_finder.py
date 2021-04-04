48b3267b8e668f94d6aa933584b33106d1a5307e
maddyboo <81530279+maddyboo@users.noreply.github.com>
2021-04-04 15:53:31 +0100

    v0.1 - proof of concept
    Proof of concept code using made up dataset ("imaginary_data") for ease.
    
    Code imports dataset, determines the percentage of daily cases related to each variant, calculates weekly percentage trend and then identifies potential variants of interest.

---
+++ dominant_variant_finder/dominant_variant_finder.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Dominant Variant Finder v0.1 (2021-04-04)
+@author: maddyboo
+"""
+
+import pandas as pd
+
+# %% data import
+# import raw daily data
+data_csv = 'Imaginary_Data.csv'
+raw_data = pd.read_csv(data_csv, parse_dates=(['Date']), index_col=('Date'),
+                       dayfirst=(True))
+
+# get list of variants (columns)
+variants_list = raw_data.columns
+
+# back-fill period before variant existed with zero cases
+backfill_data = raw_data.fillna(0)
+
+# get total cases reported that day
+backfill_data['Total'] = backfill_data.sum(axis=1)
+
+# calculate centered 7-day rolling average to smooth data
+cleaned_data = backfill_data.rolling(7, center=True).mean()
+cleaned_data.plot.line()
+
+# %% daily variant percentages
+# calculate percentage composition of each variant for that day
+
+# create blank to form table from
+pc_data = {}
+
+# loop to iterate through all the variant columns, calculating percentage
+# contribution of variant to the daily new cases total
+for variant in range(len(variants_list)):
+    variant_name = variants_list[variant]
+    percentage = (cleaned_data[variant_name]/cleaned_data['Total'])*100
+    pc_data[variant_name] = percentage
+
+# combine each column of results into a new table of variant percentage
+# contribution per day
+pc_table = pd.concat(pc_data, axis=1)
+pc_table.plot.line()
+
+# %% identify trends
+# calculate change in variant percentage over last 7 days
+pc_change_week = pc_table.diff(periods=7)
+pc_change_week.plot.line()
+
+# %% variants of interest
+# definitions:
+# - rapid increase in proportion of cases from variant
+# - above specified threshold percentage of daily reported cases
+# - other rules/definitions could be added? e.g. region to region spread?
+
+# %% rapid increase in variant
+rapid_increase_triggered = {}
+
+# loop to iterate through variants and find growth rate >5% per week
+for variant in range(len(variants_list)):
+    query = str(variants_list[variant]+" > 5")
+    rapid_date = pc_change_week.query(query).first('1D').index
+    if rapid_date.size > 0:
+        rapid_increase_triggered[variants_list[variant]] = rapid_date
+
+# combine the dates when each variant first passed rapid growth trigger
+rapid_increase_table = pd.DataFrame.from_dict(rapid_increase_triggered,
+                                              orient="index",
+                                              columns=["Rapid Trigger At"])
+
+# %% above specified threshold percentage
+threshold_triggered = {}
+
+# loop to iterate through variants and find first date above 20% threshold
+for variant in range(len(variants_list)):
+    query = str(variants_list[variant]+" > 20")
+    threshold_date = pc_table.query(query).first('1D').index
+    if threshold_date.size > 0:
+        threshold_triggered[variants_list[variant]] = threshold_date
+
+# combine the dates when each variant first passed threshold
+threshold_table = pd.DataFrame.from_dict(threshold_triggered, orient="index",
+                                         columns=["Threshold Met At"])
+
+# %% reporting
+# combine and tabulate warning markers
+variants_interest = rapid_increase_table.join(threshold_table,
+                                              how='outer')
+variants_interest["Alert Count"] = variants_interest.count(axis=1)
+
+# output variants of interest report
+print(variants_interest.sort_values(by=['Alert Count'], ascending=False))
